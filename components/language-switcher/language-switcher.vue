92766ae9341be6cb8a1a41dfcc90132782d31e20
Jason Harris <jason.harris@endsleigh.co.uk>
2019-08-21 10:18:50 +0100

    Small styling changes to the language switcher component

--- components/language-switcher/language-switcher.vue
+++ components/language-switcher/language-switcher.vue
@@ -1,10 +1,11 @@
 <template>
-  <div>
+  <div class="language-switcher">
     <img src="~/assets/images/language.svg" class="language" />
     <nuxt-link
         v-for="locale in availableLocales"
         :key="locale.code"
         :to="switchLocalePath(locale.code)"
+        :lang="locale.code"
         class="language-option">
         {{ locale.name }}
     </nuxt-link>
@@ -21,14 +22,22 @@
   };
 </script>
 
-<style>
+<style scoped>
+  .language-switcher {
+    display: flex;
+    align-items: center;
+    font-size: 12px;
+    border: 1px solid black;
+    border-radius: 3px;
+    padding: 3px;
+  }
+
   .language {
-    width: 20px;
+    width: 18px;
+    padding-right: 3px;
   }
 
   .language-option {
-    border: 1px solid black;
-    padding: 5px;
     text-decoration: none;
   }
 </style>
