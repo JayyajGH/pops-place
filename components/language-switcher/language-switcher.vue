cfef52c3405d05b0258a971e5fdbb452c4da1bc1
Jason Harris <jason.harris@endsleigh.co.uk>
2019-09-09 15:05:26 +0100

    Tidy up the class names in the language switcher

--- components/language-switcher/language-switcher.vue
+++ components/language-switcher/language-switcher.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="language-switcher">
-    <img src="~/assets/images/language.svg" class="language" />
+    <img src="~/assets/images/language.svg" class="language-switcher__image" />
     <nuxt-link
         v-for="locale in availableLocales"
         :key="locale.code"
         :to="switchLocalePath(locale.code)"
         :lang="locale.code"
-        class="language-option">
+        class="language-switcher__option">
         {{ locale.name }}
     </nuxt-link>
   </div>
@@ -32,18 +32,18 @@
     padding: 3px;
   }
 
-  .language-switcher:hover .language-option {
+  .language-switcher:hover .language-switcher__option {
     text-decoration: underline;
     text-decoration-color: var(--green-base);
     color: var(--black);
   }
 
-  .language {
+  .language-switcher__image {
     width: 18px;
     padding-right: 3px;
   }
 
-  .language-option {
+  .language-switcher__option {
     text-decoration: none;
     color: var(--grey-dark);
   }
