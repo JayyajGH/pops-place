dce69038742e071f1d2779652a64f204f71e6e16
Jason Harris <jason.harris@endsleigh.co.uk>
2019-09-12 09:20:43 +0100

    Add a test framework and dummy test

---
+++ setup.js
@@ -0,0 +1,10 @@
+const hooks = require('require-extension-hooks');
+
+// Set up a virtual browser environment.
+require('jsdom-global')();
+window.Date = Date;
+
+// Setup `.vue` files to be processed by `require-extension-hooks-vue`.
+hooks('vue').plugin('vue').push();
+// Setup `.vue` and `.js` files to be processed by `require-extension-hooks-babel`.
+hooks(['vue', 'js']).plugin('babel').push()
