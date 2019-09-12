fbd198443c75d89591c801888c5c6ce253f86930
Jason Harris <jason.harris@endsleigh.co.uk>
2019-09-12 11:19:34 +0100

    Add module-alias to set up aliases for tests in the absence of webpack

--- setup.js
+++ setup.js
@@ -8,3 +8,7 @@ window.Date = Date;
 hooks('vue').plugin('vue').push();
 // Setup `.vue` and `.js` files to be processed by `require-extension-hooks-babel`.
 hooks(['vue', 'js']).plugin('babel').push()
+
+// Without the webpack config then any aliases will need to be set up here
+var module = require('module-alias');
+module.addAlias('@', __dirname + '/src');
